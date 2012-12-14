e6eb388d860739132ea1319c06aaf9b96ab6ce49
Gravitation <gravi@tation.tk>
2012-12-14 06:57:07 +0200

    Cleaned up my engine a bit

---
+++ eapdef.py
@@ -0,0 +1,88 @@
+Sprites = []
+Scripts = []
+Blocks = []
+class Sprite():
+    global Sprites, Scripts
+    def __init__(self):
+        self.x = 0
+        self.y = 0
+        self.dir = 90
+        self.hidden = False
+        self.myScripts = []
+        self.name = "Sprite1"
+    def run(self):
+        for i in self.myScripts:
+            i.run()
+class Script():
+    global Sprites, Scripts, Blocks
+    def __init__(self):    
+        global Sprites
+        self.id = 0
+        self.loops = []
+        self.myBlocks = []
+        self.pos = 0
+        self.owner = 0
+        k = 0
+        for i in Sprites:
+            for j in i.myScripts:
+                if j.id == self.id:
+                    self.owner = k
+                k += 1
+    def run(self):
+        if self.pos == -1:
+            if self.loops[len(self.loops) - 1][2] == len(self.myBlocks):
+                self.pos = self.loops[len(self.loops) - 1][1]
+            else:
+                if self.myBlocks[0].run() == True:
+                    self.pos == 0
+        if self.pos == 0 and self.myBlocks[0].run() == True:
+            self.pos = 1
+        self.myBlocks[pos].run()
+class Block():
+    global Scripts, Blocks
+    def __init__(self):
+        global Scripts
+        self.id = 0
+        self.block = 0
+        self.inputs = []
+        self.complete = False
+        self.running = False
+        k = 0
+        for i in Scripts:
+            for j in i.myBlocks:
+                if j.id == self.id:
+                    self.owner = k
+                    k += 1
+    def check(self):
+        global Scripts
+        if self.running == False:
+            Scripts[self.owner].pos += 1
+            if (Scripts[self.owner].pos + 1) > len(Scripts[self.owner].myBlocks):
+                Scripts[self.owner].pos = -1
+    def run(self):
+        global Sprites, answer
+        for i in range(len(self.inputs)):
+            if type(self.inputs[i]) == type(Sprites[0]):
+                self.inputs[i] = self.inputs[i].run()
+        if self.block == 0: #say
+            string = str(self.inputs[0])
+            print(string)
+        if self.block == 1: #ask
+            string = str(self.inputs[0])
+            answer = raw_input(string)
+        from math import sqrt
+        if self.block == 2: #join
+            a = str(self.inputs[0])
+            b = str(self.inputs[1])
+            return a + b
+        if self.block == 3: #mod
+            return self.inputs[0] % self.inputs[1]
+        if self.block == 4: #letter of
+            string = str(self.inputs[1])
+            num = (self.inputs[0] - 1) % len(string)
+            return string[num]
+        if self.block == 5: #length of
+            string = str(self.inputs[0])
+            return len(string)
+if __name__ == "__main__":
+    print "Block definitions and paralell scripting for EasyAsPy."
--- easyaspy.py
+++ easyaspy.py
@@ -1,39 +1,4 @@
-import scratch
-print('[Easy As Py]')
-#Looks
-def say(s):
-	s = str(s)
-	print(s)
-#Sensing
-def ask(s):
-	global answer
-	s = str(s)
-	answer = raw_input(s)
-#Operators
-from math import sqrt
-def join(a, b):
-	a = str(a)
-	b = str(b)
-	return a + b
-def mod(a, b):
-	return a % b
-def letter_of(num, s):
-	s = str(s)
-	num = num - 1
-	return s[num]
-def length_of(s):
-	s = str(s)
-	return len(s)
-#Control
-def broadcast(s):
-	scratchpy.broadcast(s)
-def connect():
-	ask('Connect to Scratch using default connection? (Yes or No)')
-	answer = answer.lower()
-	if answer == 'yes':
-		scratchpy = scratch.Scratch(host='localhost')
-	elif answer == 'no':
-		ask('What IP would you like to connect to?')
-		scratchpy = scratch.Scratch(host=answer)
-	else:
-		print('Response not understood.')
+import sys, pygame
+from pygame.locals import *
+from eapdef import *
+#Note: Block definitions are in the eapdef file. Too lazy to implement broadcasting in the Scratch script engine.
